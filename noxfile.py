be3f4902a9a03d3bc94f201dc5922bafe12e0445
debra-keiser <144846430+debra-keiser@users.noreply.github.com>
2023-10-28 12:10:02 -0400

    Add gitignore, pre-commit, toml, nox, setup files

---
+++ noxfile.py
@@ -0,0 +1,12 @@
+import nox
+
+nox.options.sessions = ["tests"]
+
+
+@nox.session
+def tests(session):
+    """
+    Run all defined tests.
+    """
+    session.install(".[test]")
+    session.run("pytest", *session.posargs)
