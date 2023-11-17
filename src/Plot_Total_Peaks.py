ff95c31cf091565f7cd80a41485a75e4539c9012
debra-keiser <144846430+debra-keiser@users.noreply.github.com>
2023-11-17 18:12:26 -0500

    Update Plot_Total_Peaks.py

--- src/Plot_Total_Peaks.py
+++ src/Plot_Total_Peaks.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-npz_file = "pdf_ramp_peaks.npz"
-
 
 def plot_total_peaks(npz_file):
     """
@@ -14,34 +12,35 @@ def plot_total_peaks(npz_file):
     Returns:
         Histogram showing the total number of peaks for each PDF file.
     """
-    my_dict = np.load(npz_file)
-    new_dict = {}
+    data_from_npz = np.load(npz_file)
+    npz_dict = {}
 
-    # Extract keys and values from npz and put in a new dictionary
-    for item in my_dict:
-        new_dict[item] = my_dict[item]
+    # Extract keys and values from npz and store in a dictionary.
+    for item in  data_from_npz:
+        npz_dict[item] = data_from_npz[item]
 
-    # Make a dictionary where the key is file name, value is number of peaks
-    num_peaks = {}
-    for key in new_dict:
-        num_peaks[key] = len(my_dict[key])
+    # Make a dictionary where the key is file name, value is number of peaks.
+    number_of_peaks_dict = {}
+    for key in npz_dict:
+        number_of_peaks_dict[key] = len(data_from_npz[key])
 
-    # Select which keys to label on the x-axis (e.g., every 10th key)
-    keys_to_label = list(num_peaks.keys())[::10]
+    # Select which keys to label on the x-axis (e.g., every 10th key).
+    keys_to_label = list(number_of_peaks_dict.keys())[::10]
 
-    # Display all keys in a historgram
-    keys_to_display = list(num_peaks.keys())
-    values_to_display = [num_peaks[key] for key in keys_to_display]
+    # Display all keys and values in a historgram.
+    keys_to_display = list(number_of_peaks_dict.keys())
+    values_to_display = [number_of_peaks_dict[key] for key in keys_to_display]
 
-    plt.bar(range(len(keys_to_display)), values_to_display, color="g")
+    plt.bar(range(len(keys_to_display)), values_to_display, color = "g")
     plt.xlabel("File Name")
     plt.ylabel("Number of Peaks")
 
-    # Label only the selected keys for readability
+    # Label only the selected keys for readability.
     plt.xticks(
         range(len(keys_to_display)),
         [key if key in keys_to_label else "" for key in keys_to_display],
-        rotation=90,
+        rotation = 90,
     )
-    plt.xticks(rotation=45)
-    plt.show()
+    plt.xticks(rotation = 45)
+    plt.tight_layout()
+    plt.savefig('Total_Ramp_Peaks.png')
