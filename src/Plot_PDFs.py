dfd60d25e9d9b5035bbd68d1db88849b5054355a
sb5213 <sb5213@princeton.edu>
2023-12-02 18:00:25 -0500

    Add plot_PDF_peaks file

--- src/Plot_PDFs.py
+++ src/Plot_PDFs.py
@@ -23,11 +23,18 @@ def plot_peaks(r_list, g_r_list, labels):
     plt.grid(True)
     plt.show()
 
+
 # Load data for the four PDFs
 r1, g_r1 = extract_pdf_data("../data/gr_files", "Synthetic_CSH_030degC_normalized.gr")
-r2, g_r2 = extract_pdf_data("../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_300_10_normalized.gr")
-r3, g_r3 = extract_pdf_data("../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_600_10_normalized.gr")
-r4, g_r4 = extract_pdf_data("../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_1000_10_normalized.gr")
+r2, g_r2 = extract_pdf_data(
+    "../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_300_10_normalized.gr"
+)
+r3, g_r3 = extract_pdf_data(
+    "../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_600_10_normalized.gr"
+)
+r4, g_r4 = extract_pdf_data(
+    "../data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_1000_10_normalized.gr"
+)
 
 # Call plot_peaks with the four datasets
 plot_peaks([r1, r2, r3, r4], [g_r1, g_r2, g_r3, g_r4], ["30C", "300C", "600C", "1000C"])
