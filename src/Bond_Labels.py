7ae62f3ac6af4b3245e08d8cc14a0fe5fc8ee6c8
sb5213 <sb5213@princeton.edu>
2023-12-06 04:40:47 -0500

    Bond Labels Function

--- src/Bond_Labels.py
+++ src/Bond_Labels.py
@@ -13,9 +13,8 @@ Returns:
                 with keys as (bond, bond_length) and values as corresponding G(r) values.
 """
 
-import matplotlib.pyplot as plt
-import numpy as np
 from Extract_Data import locate_peaks
+
 
 def extract_peak_labels(r1, g_r1):
     """
@@ -30,20 +29,22 @@ def extract_peak_labels(r1, g_r1):
     - labels (dict): Dictionary containing labels for peaks within the first five Angstroms,
                      with keys as (bond, bond_length) and values as corresponding G(r) values.
     """
-    
+
     peaks = locate_peaks(g_r1)  # You can adjust the 'height' parameter as needed
 
     # Known peak length ranges
     known_peak_ranges = {
-        'Si-O': (1.5, 1.7),
-        'Ca-O': (2.3, 2.5),
-        'O-O': (2.6, 2.7),
-        'Si-Si': (3, 3.2)
+        "Si-O": (1.5, 1.7),
+        "Ca-O": (2.3, 2.5),
+        "O-O": (2.6, 2.7),
+        "Si-Si": (3, 3.2),
     }
     # Match identified peaks with known bond length ranges
     matched_peaks = {}
     for bond, (lower, upper) in known_peak_ranges.items():
-        matched_indices = [i for i, val in enumerate(r1[peaks]) if lower <= val <= upper]
+        matched_indices = [
+            i for i, val in enumerate(r1[peaks]) if lower <= val <= upper
+        ]
         matched_peaks[bond] = matched_indices
 
     labels = {}
@@ -51,6 +52,8 @@ def extract_peak_labels(r1, g_r1):
         for idx in indices:
             if r1[peaks][idx] <= 5:  # Consider peaks within the first five Angstroms
                 bond_length = r1[peaks][idx]  # Retrieve the bond length
-                labels[(bond, bond_length)] = g_r1[peaks][idx]  # Save bond and length in the dictionary
+                labels[(bond, bond_length)] = g_r1[peaks][
+                    idx
+                ]  # Save bond and length in the dictionary
 
     return labels
