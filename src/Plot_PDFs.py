dbbb2d9b3cb1c55d61c202bc75a4f7f690d94807
sb5213 <sb5213@princeton.edu>
2023-12-06 08:58:42 -0500

    small edit: fix file path

--- src/Plot_PDFs.py
+++ src/Plot_PDFs.py
@@ -5,13 +5,12 @@ Author: Sophia Bergen
 Date Modified: 06DEC2023
 
 Description:
-This file contains all functions related to plotting the PDF data
+This file contains all functions related to plotting the PDF data 
 """
 
 import matplotlib.pyplot as plt
+from Extract_Data import extract_pdf_data, locate_peaks, rescale_g_r
 from Bond_Labels import extract_peak_labels
-from Extract_Data import extract_pdf_data, rescale_g_r
-
 
 def plot_peaks(r_list, g_r_list, labels, filename):
     """
@@ -36,9 +35,8 @@ def plot_peaks(r_list, g_r_list, labels, filename):
     plt.legend()
     plt.grid(True)
     # Save the plot as a PNG file
-    plt.savefig(filename, format="png")
+    plt.savefig(filename, format='png')
     plt.close()  # Close the plot to release memory
-
 
 def plot_zoomed_peaks(r_list, g_r_list, legend_labels, filename):
     """
@@ -64,13 +62,7 @@ def plot_zoomed_peaks(r_list, g_r_list, legend_labels, filename):
 
     # Plot bond lengths within the first five Angstroms
     for bond_length in labels.keys():
-        plt.text(
-            bond_length[1],
-            labels[bond_length],
-            f"{bond_length[0]}",
-            ha="center",
-            va="bottom",
-        )
+        plt.text(bond_length[1], labels[bond_length], f"{bond_length[0]}", ha='center', va='bottom')
 
     plt.title("PDF Data (0 to 5 Angstroms) at Increasing Temperatures", fontsize=16)
     plt.xlabel("r (\u00C5)", fontsize=14)
@@ -78,9 +70,8 @@ def plot_zoomed_peaks(r_list, g_r_list, legend_labels, filename):
     plt.grid(True)
     plt.xlim(0, 5)
     plt.legend()
-    plt.savefig(filename, format="png")
+    plt.savefig(filename, format='png')
     plt.close()  # Close the plot to release memory
-
 
 def Plot_multiple_PDFs():
     """
@@ -93,39 +84,31 @@ def Plot_multiple_PDFs():
     - total_plot_filename (str): Filename or path of the saved PNG file containing the original plot.
     - zoomed_plot_filename (str): Filename or path of the saved PNG file containing the zoomed-in plot.
     """
-    r1, g_r1 = extract_pdf_data(
-        "./data/gr_files", "Synthetic_CSH_030degC_normalized.gr"
-    )
-    r2, g_r2 = extract_pdf_data(
-        "./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_300_09_normalized.gr"
-    )
-    r3, g_r3 = extract_pdf_data(
-        "./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_600_09_normalized.gr"
-    )
-    r4, g_r4 = extract_pdf_data(
-        "./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_1000_09_normalized.gr"
-    )
+    r1, g_r1 = extract_pdf_data("./data/gr_files", "Synthetic_CSH_030degC_normalized.gr")
+    r2, g_r2 = extract_pdf_data("./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_300_09_normalized.gr")
+    r3, g_r3 = extract_pdf_data("./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_600_09_normalized.gr")
+    r4, g_r4 = extract_pdf_data("./data/gr_files", "Synthetic_CSH_CSH_pdf_ramp_1000_09_normalized.gr")
 
     g_r1 = rescale_g_r(g_r1)
     # Call plot_peaks with the datasets for the full r-range plot.
-    total_plot_filename = "total_peaks_and_PDFs.png"
+    total_plot_filename = "./data/total_peaks_and_PDFs.png"
     plot_peaks(
         [r1, r2, r3, r4],
         [g_r1, g_r2, g_r3, g_r4],
         ["30\u00B0C", "300\u00B0C", "600\u00B0C", "1000\u00B0C"],
-        total_plot_filename,
+        total_plot_filename
     )
 
     # Call plot_zoomed_peaks with the four datasets for the zoomed-in plot.
-    zoomed_plot_filename = "zoomed_peaks_and_PDFs2.png"
+    zoomed_plot_filename = "./data/zoomed_peaks_and_PDFs2.png"
     plot_zoomed_peaks(
         [r1, r2, r3, r4],
         [g_r1, g_r2, g_r3, g_r4],
         ["30\u00B0C", "300\u00B0C", "600\u00B0C", "1000\u00B0C"],
-        zoomed_plot_filename,
+        zoomed_plot_filename
     )
 
-    return (
-        total_plot_filename,
-        zoomed_plot_filename,
-    )  # Return filenames or paths of the saved PNG files
+    return total_plot_filename, zoomed_plot_filename  # Return filenames or paths of the saved PNG files
+
+
+
