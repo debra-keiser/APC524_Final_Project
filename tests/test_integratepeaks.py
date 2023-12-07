1509ccf16d9ceba51269f100617dfbaa9d59044b
Debra Keiser <dkeiser@princeton.edu>
2023-12-07 14:14:36 -0500

    Add test for Integrate_Peaks.py

--- tests/test_integratepeaks.py
+++ tests/test_integratepeaks.py
@@ -1,22 +1,19 @@
-from src.Integrate_Peaks import (
-    scale_peak_integrals,
-    peak_integral_differences,
-)
-
-
-
-
-
-
 import numpy as np
 
 from src.Extract_Data import extract_pdf_data
+from src.Integrate_Peaks import (
+    peak_integral_differences,
+    scale_peak_integrals,
+)
 
 
 def test_integrate_peak_areas():
     """Check that the correct lower- and upper-bound peak minimums are identified."""
-    _, pdf_dwell_data_g_r = extract_pdf_data("../data/gr_files", f"Synthetic_CSH_100degC_normalized.gr",)
-    
+    _, pdf_dwell_data_g_r = extract_pdf_data(
+        "../data/gr_files",
+        "Synthetic_CSH_100degC_normalized.gr",
+    )
+
     i_lower = 1
     min_lower_bound = pdf_dwell_data_g_r[163]
     while min_lower_bound > pdf_dwell_data_g_r[163 - i_lower]:
@@ -35,14 +32,30 @@ def test_integrate_peak_areas():
 
 def test_scale_peak_integrals():
     """Check that the peak integrals are multiplied by the proper scale factor."""
-    mock_scaled_peak_integrals_dict = scale_peak_integrals({100:np.array([5]), 200:np.array([7])})
+    mock_scaled_peak_integrals_dict = scale_peak_integrals(
+        {100: np.array([5]), 200: np.array([7])}
+    )
 
     assert mock_scaled_peak_integrals_dict[200] == 5
 
 
 def test_peak_integral_differences():
     """Check that the expected reference and relative dictionaries are returned."""
-    mock_reference_peak_integrals_dict, mock_peak_integrals_differences_dict = peak_integral_differences({100:np.array([5, 6, 7]), 200:np.array([7, 8, 9]), 600:np.array([9, 10, 11])}, {100:np.array([150, 300, 550]), 200:np.array([160, 305, 325]), 600:np.array([170, 310, 400])})
+    (
+        mock_reference_peak_integrals_dict,
+        mock_peak_integrals_differences_dict,
+    ) = peak_integral_differences(
+        {
+            100: np.array([5, 6, 7]),
+            200: np.array([7, 8, 9]),
+            600: np.array([9, 10, 11]),
+        },
+        {
+            100: np.array([150, 300, 550]),
+            200: np.array([160, 305, 325]),
+            600: np.array([170, 310, 400]),
+        },
+    )
 
-    assert mock_reference_peak_integrals_dict == {150:5, 300:6}
+    assert mock_reference_peak_integrals_dict == {150: 5, 300: 6}
     assert len(mock_peak_integrals_differences_dict) == 2
