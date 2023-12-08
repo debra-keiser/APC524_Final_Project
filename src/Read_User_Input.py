378072c0e5aaf84adc971cf75fd68479f33af225
medimut <ms8898@princeton.edu>
2023-12-08 17:09:54 -0500

    Modify gitignore, add user_input.txt

--- src/Read_User_Input.py
+++ src/Read_User_Input.py
@@ -9,8 +9,10 @@ This script reads user_input.txt to feed any functions with PDF files intended t
 Returns 2 separate arrays, one indicating the type of experiment (either 'dwell' or 'ramp') and the other indicating the key.
 """
 import numpy as np
+
+
 def read_user_input():
-    user_input = open("user_input.txt","r")
+    user_input = open("user_input.txt")
     experiment_type = np.array([])
     temperature_point = np.array([])
 
@@ -18,6 +20,6 @@ def read_user_input():
         fields = line.split(",")
         experiment_type = np.append(experiment_type, fields[0])
         temperature_point = np.append(temperature_point, fields[1][:-1])
-    
+
     user_input.close()
     return experiment_type, temperature_point
