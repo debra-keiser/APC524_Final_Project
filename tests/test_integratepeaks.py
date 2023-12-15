9886cd6ffdd032b11cc8c83e5d73b85cd2d2e097
Debra Keiser <dkeiser@princeton.edu>
2023-12-15 14:06:33 -0500

    Update test for peak_integral_differences function

--- tests/test_integratepeaks.py
+++ tests/test_integratepeaks.py
@@ -42,7 +42,7 @@ def test_scale_peak_integrals():
 def test_peak_integral_differences():
     """Check that the expected reference and relative dictionaries are returned."""
     (
-        mock_number_of_integrated_peaks,
+        mock_reference_peak_positions_list,
         mock_peak_integrals_differences_dict,
     ) = peak_integral_differences(
         {
@@ -57,5 +57,5 @@ def test_peak_integral_differences():
         },
     )
 
-    assert mock_number_of_integrated_peaks == 2
+    assert mock_reference_peak_positions_list == [150, 300]
     assert len(mock_peak_integrals_differences_dict) == 2
